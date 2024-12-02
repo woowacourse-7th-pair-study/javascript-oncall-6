7b6d7df40f6583f3bc3984c3c20962aecb7323cb
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2024-12-02 14:02:19 +0900

    feat: 근무 닉네임이 중복될 경우 예외 처리

--- src/validator/validateWeekdayStaffInput.js
+++ src/validator/validateWeekdayStaffInput.js
@@ -1,7 +1,10 @@
 import { throwWoowaError } from '../util/error.js';
-import { isInRange } from '../util/validator.js';
+import { isDuplicate, isInRange } from '../util/validator.js';
 
 export const validateWeekdayStaffInput = (input) => {
   if (!isInRange(input.length, 5, 35))
     throwWoowaError('근무 인원수가 5보다 작고, 35보다 크면 안됩니다.');
+
+  if (isDuplicate(input))
+    throwWoowaError('비상 근무자는 평일 순번에 1회만 편성되어야 한다.');
 };
