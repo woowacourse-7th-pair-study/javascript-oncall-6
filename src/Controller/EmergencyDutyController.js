82c36322370f0d4e032755e65a6332b3538f7dd5
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2024-12-02 13:34:39 +0900

    refactor: parseInput 함수 추출

--- src/Controller/EmergencyDutyController.js
+++ src/Controller/EmergencyDutyController.js
@@ -1,13 +1,15 @@
 import { Console } from '@woowacourse/mission-utils';
 import Input from '../View/Input.js';
+import { parseInputWithSeparator } from '../parser/parseInput.js';
+import { INPUT_SEPARATOR } from '../constant/seperator.js';
 
 class EmergencyDutyController {
   constructor() {}
 
   async init() {
     const startMonthAndDay = await Input.getStartMonthAndDay()((input) => {
-      const parsedStartMonthAndDay = input.split(',');
-      return parsedStartMonthAndDay;
+      const parsedInput = parseInputWithSeparator(input, INPUT_SEPARATOR);
+      return parsedInput;
     });
 
     Console.print(startMonthAndDay);
---
+++ src/parser/parseInput.js
@@ -0,0 +1,2 @@
+export const parseInputWithSeparator = (input, separator) =>
+  input.split(separator);
