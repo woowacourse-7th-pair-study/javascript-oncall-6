c775f7324421d2ceb1ba690bc6e7cf46e0385aa1
jiyeeeah <sije3757@snu.ac.kr>
2024-12-02 13:59:41 +0900

    chore: 법정공휴일 상수화

--- src/constant/daysInfo.js
+++ src/constant/daysInfo.js
@@ -1,1 +1,12 @@
 export const DAY_OF_WEEK = ['일', '월', '화', '수', '목', '금', '토'];
+
+export const HOLIDAY = [
+  { month: 1, day: 1 },
+  { month: 3, day: 1 },
+  { month: 5, day: 5 },
+  { month: 6, day: 6 },
+  { month: 8, day: 15 },
+  { month: 10, day: 3 },
+  { month: 10, day: 9 },
+  { month: 12, day: 25 },
+];
