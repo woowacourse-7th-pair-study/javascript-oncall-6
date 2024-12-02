96745662e325a11bc787126c6f4dc66efa7433e9
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2024-12-02 13:36:43 +0900

    feat: ,로 분리했을 때, 길이가 두 개가 아닐 경우 예외 처리

--- src/Controller/EmergencyDutyController.js
+++ src/Controller/EmergencyDutyController.js
@@ -2,6 +2,7 @@ import { Console } from '@woowacourse/mission-utils';
 import Input from '../View/Input.js';
 import { parseInputWithSeparator } from '../parser/parseInput.js';
 import { INPUT_SEPARATOR } from '../constant/seperator.js';
+import { throwWoowaError } from '../util/error.js';
 
 class EmergencyDutyController {
   constructor() {}
@@ -9,6 +10,8 @@ class EmergencyDutyController {
   async init() {
     const startMonthAndDay = await Input.getStartMonthAndDay()((input) => {
       const parsedInput = parseInputWithSeparator(input, INPUT_SEPARATOR);
+      if (parsedInput.length !== 2)
+        throwWoowaError('유효하지 않은 입력 값입니다.');
       return parsedInput;
     });
 
---
+++ src/util/error.js
@@ -0,0 +1,3 @@
+export const throwWoowaError = (message) => {
+  throw new Error(`[ERROR] ${message} 다시 입력해주세요.`);
+};
