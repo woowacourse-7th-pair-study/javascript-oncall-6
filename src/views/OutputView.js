14bbda20844204541a42787d2580924056fc321a
sooyeoniya <tndus1306403@naver.com>
2024-12-02 14:25:14 +0900

    feat(OutputView): 비상 근무표 출력 구현

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,9 +1,15 @@
 import { Console } from '@woowacourse/mission-utils';
 
 const OutputView = {
-  // print() {
-  //   Console.print('출력 예시\n');
-  // },
+  /**
+   * 비상 근무표를 출력한다.
+   * @param {Array<{ month: number, day: number, dayWeek: string, name: string }>} schedule 
+   */
+  printSchedule(schedule) {
+    schedule.forEach((byDay) => {
+      Console.print(`${byDay.month}월 ${byDay.day}일 ${byDay.dayWeek} ${byDay.name}`);
+    });
+  },
 
   /**
    * 에러 메시지와 함께 에러를 던진다.
