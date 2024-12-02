cd095ccef210c0c0cdae8a6a67f596b65c59f277
Parksangwoo <psu8430@gmail.com>
2024-12-02 16:34:03 +0900

    feat(WorkScheduler) : 연속 근무자 일정 조율 로직 추가

--- src/Models/WorkScheduler.js
+++ src/Models/WorkScheduler.js
@@ -5,6 +5,9 @@ class WorkScheduler {
     const { weekDayWorkers, holidayWorkers } = workerInfo;
     const workSchedule = [''];
 
+    let weekDayQueue = [];
+    let holidayQueue = [];
+
     let curWeekNameIndex = WEEK_NAME.indexOf(day);
 
     let weekDayWorkersIndex = 0;
@@ -13,13 +16,36 @@ class WorkScheduler {
     for (let curDay = 1; curDay <= MONTH_DAYS[month]; curDay += 1) {
       // 평일 근무자 삽입
       if (curWeekNameIndex < 5 && !HOLIDAY_INFO[month].includes(curDay)) {
-        workSchedule.push(weekDayWorkers[weekDayWorkersIndex]);
-        weekDayWorkersIndex = (weekDayWorkersIndex + 1) % weekDayWorkers.length;
+        if (weekDayWorkers[weekDayWorkersIndex] === workSchedule[curDay - 1]) {
+          // 전일 근무자와 같은 경우
+          weekDayQueue = [...weekDayQueue, weekDayWorkers[weekDayWorkersIndex]];
+          weekDayWorkersIndex = (weekDayWorkersIndex + 1) % weekDayWorkers.length;
+          workSchedule.push(weekDayWorkers[weekDayWorkersIndex]);
+        } else {
+          if (weekDayQueue.length) {
+            const delayedWorker = weekDayQueue.pop();
+            workSchedule.push(delayedWorker);
+          } else {
+            workSchedule.push(weekDayWorkers[weekDayWorkersIndex]);
+            weekDayWorkersIndex = (weekDayWorkersIndex + 1) % weekDayWorkers.length;
+          }
+        }
       }
       // 휴일 근무자 삽입
       else {
-        workSchedule.push(holidayWorkers[holidayWorkersIndex]);
-        holidayWorkersIndex = (holidayWorkersIndex + 1) % holidayWorkers.length;
+        if (holidayWorkers[holidayWorkersIndex] === workSchedule[curDay - 1]) {
+          holidayQueue = [...holidayQueue, holidayWorkers[holidayWorkersIndex]];
+          holidayWorkersIndex = (holidayWorkersIndex + 1) % holidayWorkers.length;
+          workSchedule.push(holidayWorkers[holidayWorkersIndex]);
+        } else {
+          if (holidayQueue.length) {
+            const delayedWorker = holidayQueue.pop();
+            workSchedule.push(delayedWorker);
+          } else {
+            workSchedule.push(holidayWorkers[holidayWorkersIndex]);
+            holidayWorkersIndex = (holidayWorkersIndex + 1) % holidayWorkers.length;
+          }
+        }
       }
 
       curWeekNameIndex = (curWeekNameIndex + 1) % WEEK_NAME.length;
