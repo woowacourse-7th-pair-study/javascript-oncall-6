714cbb2cbba9876ad12993407b1df22bc79fc45c
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2024-12-02 14:04:58 +0900

    feat: 근무 닉네임이 5 글자를 초과할 경우 예외 처리

--- src/validator/validateWeekdayStaffInput.js
+++ src/validator/validateWeekdayStaffInput.js
@@ -1,10 +1,15 @@
 import { throwWoowaError } from '../util/error.js';
 import { isDuplicate, isInRange } from '../util/validator.js';
 
-export const validateWeekdayStaffInput = (input) => {
-  if (!isInRange(input.length, 5, 35))
+export const validateWeekdayStaffInput = (staffNamesInput) => {
+  if (!isInRange(staffNamesInput.length, 5, 35))
     throwWoowaError('근무 인원수가 5보다 작고, 35보다 크면 안됩니다.');
 
-  if (isDuplicate(input))
+  if (isDuplicate(staffNamesInput))
     throwWoowaError('비상 근무자는 평일 순번에 1회만 편성되어야 한다.');
+
+  staffNamesInput.forEach((staffName) => {
+    if (!isInRange(staffName.length, 1, 5))
+      throwWoowaError('근무 닉네임이 1자 이상, 5자 이하여야 합니다.');
+  });
 };
