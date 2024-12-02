cd32afbcd6db3bb77ab54c853568a3ee38781443
sooyeoniya <tndus1306403@naver.com>
2024-12-02 14:14:11 +0900

    feat(constants): 법적 공휴일 및 요일 상수화

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -5,3 +5,28 @@ export const INPUT_MESSAGES = Object.freeze({
   WEEKDAY: '평일 비상 근무 순번대로 사원 닉네임을 입력하세요> ',
   WEEKEND: '휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> ',
 });
+
+export const HOLIDAY = Object.freeze({
+  1: [ 1 ],
+  3: [ 1 ],
+  5: [ 5 ],
+  6: [ 6 ],
+  8: [ 15 ],
+  10: [ 3, 9 ],
+  12: [ 25 ],
+});
+
+export const LAST_DAY = Object.freeze({
+  1: 31,
+  2: 28,
+  3: 31,
+  4: 30,
+  5: 31,
+  6: 30,
+  7: 31,
+  8: 31,
+  9: 30,
+  10: 31,
+  11: 30,
+  12: 31,
+});
