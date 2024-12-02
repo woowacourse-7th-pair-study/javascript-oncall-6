f08e47485410316f96f6f6605682c593656de538
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2024-12-02 13:42:18 +0900

    feat: 입력 받은 월 정보가 1 ~ 12 사이가 아닐 경우 예외 처리

--- src/Controller/EmergencyDutyController.js
+++ src/Controller/EmergencyDutyController.js
@@ -18,6 +18,11 @@ class EmergencyDutyController {
 
       if (!Number.isInteger(Number(parsedInput[0])))
         throwWoowaError('입력 받은 월 정보가 정수가 아닙니다.');
+
+      const isInRange = (value, min, max) => value >= min && value <= max;
+
+      if (!isInRange(Number(parsedInput[0]), 1, 12))
+        throwWoowaError('입력 받은 월 정보가 1 ~ 12 사이여야 합니다.');
       return parsedInput;
     });
 
