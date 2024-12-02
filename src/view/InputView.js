0d2e51509ade0b89798fbedc27e34bfdb155ad6b
ohgus <123801385+ohgus@users.noreply.github.com>
2024-12-02 16:45:30 +0900

    refactor: input을 반환 전에 split을 진행하게 수정

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -6,19 +6,19 @@ const InputView = {
   async readMonthAndDay() {
     const input = await Console.readLineAsync(MESSAGE.month_day);
 
-    return input;
+    return input.split(',');
   },
 
   async readWeekdayOrder() {
     const input = await Console.readLineAsync(MESSAGE.weekday_order);
 
-    return input;
+    return input.split(',');
   },
 
   async readWeekendOrder() {
     const input = await Console.readLineAsync(MESSAGE.weekend_order);
 
-    return input;
+    return input.split(',');
   },
 };
 
