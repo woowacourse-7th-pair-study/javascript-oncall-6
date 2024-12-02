cc432b50406734d34ec5211c1e551e36fd1a1c8f
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2024-12-02 15:26:12 +0900

    feat: 연속 2일 근무할 경우 근무 순서 변경

--- src/Model/EmergencyDutyMachine.js
+++ src/Model/EmergencyDutyMachine.js
@@ -1,6 +1,8 @@
 class EmergencyDutyMachine {
   #weekdayStaff;
   #weekendStaff;
+  #curWeekdayStaffOrder;
+  #curWeekendStaffOrder;
   #curWeekdayIndex;
   #curWeekendIndex;
   #prevDutyStaff;
@@ -8,23 +10,45 @@ class EmergencyDutyMachine {
   constructor(weekdayStaff, weekendStaff) {
     this.#weekdayStaff = weekdayStaff;
     this.#weekendStaff = weekendStaff;
+    this.#curWeekdayStaffOrder = weekdayStaff;
+    this.#curWeekendStaffOrder = weekendStaff;
     this.#curWeekdayIndex = 0;
     this.#curWeekendIndex = 0;
   }
 
   assignDutyStaff(isHoliday) {
     if (isHoliday) {
-      const weekendDutyStaff = this.#weekendStaff[this.#curWeekendIndex];
+      let weekendDutyStaff = this.#curWeekendStaffOrder[this.#curWeekendIndex];
+      if (weekendDutyStaff === this.#prevDutyStaff) {
+        [
+          this.#curWeekendStaffOrder[this.#curWeekendIndex],
+          this.#curWeekendStaffOrder[this.#curWeekendIndex + 1],
+        ] = [
+          this.#curWeekendStaffOrder[this.#curWeekendIndex + 1],
+          this.#curWeekendStaffOrder[this.#curWeekendIndex],
+        ];
+        weekendDutyStaff = this.#curWeekendStaffOrder[this.#curWeekendIndex];
+      }
       this.#curWeekendIndex += 1;
-      if (this.#curWeekendIndex === this.#weekendStaff.length)
+      if (this.#curWeekendIndex === this.#curWeekendStaffOrder.length)
         this.#curWeekendIndex = 0;
       this.#prevDutyStaff = weekendDutyStaff;
       return weekendDutyStaff;
     }
 
-    const weekdayDutyStaff = this.#weekdayStaff[this.#curWeekdayIndex];
+    let weekdayDutyStaff = this.#curWeekdayStaffOrder[this.#curWeekdayIndex];
+    if (weekdayDutyStaff === this.#prevDutyStaff) {
+      [
+        this.#curWeekdayStaffOrder[this.#curWeekdayIndex],
+        this.#curWeekdayStaffOrder[this.#curWeekdayIndex + 1],
+      ] = [
+        this.#curWeekdayStaffOrder[this.#curWeekdayIndex + 1],
+        this.#curWeekdayStaffOrder[this.#curWeekdayIndex],
+      ];
+      weekdayDutyStaff = this.#curWeekdayStaffOrder[this.#curWeekdayIndex];
+    }
     this.#curWeekdayIndex += 1;
-    if (this.#curWeekdayIndex === this.#weekdayStaff.length)
+    if (this.#curWeekdayIndex === this.#curWeekdayStaffOrder.length)
       this.#curWeekdayIndex = 0;
     this.#prevDutyStaff = weekdayDutyStaff;
     return weekdayDutyStaff;
