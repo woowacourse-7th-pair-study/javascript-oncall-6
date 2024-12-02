3df717cfcc3f1698c24f03d8affa743d9d5e6edb
sooyeoniya <tndus1306403@naver.com>
2024-12-02 13:24:26 +0900

    docs: README.md 기능 목록 정리 및 폴더 구조 잡기

---
+++ src/constants/constants.js
@@ -0,0 +1,10 @@
+
+export const INPUT_MESSAGES = Object.freeze({
+  DEFAULT: '비상 근무를 배정할 월과 시작 요일을 입력하세요>',
+  WEEKDAY: '평일 비상 근무 순번대로 사원 닉네임을 입력하세요>',
+  WEEKEND: '휴일 비상 근무 순번대로 사원 닉네임을 입력하세요>',
+});
+
+export const ERROR_MESSAGE = Object.freeze({
+  INVALID: '[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요.',
+});
